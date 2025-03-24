f45c4ca3c7d85515a67e1465f163c6f3232da187
Angela Capillo <hello@angelcapillo.com>
2025-03-24 17:20:03 +0100

    Add functionality for search bar

--- index.js
+++ index.js
@@ -3,22 +3,17 @@ import createCharacterCard from "./components/CharacterCard/CharacterCard.js";
 // States
 let maxPage = 1;
 let page = 1;
-const searchQuery = "";
-
-//console.log(newCharacterCard);
+let searchQuery = "";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-//cardContainer.append(newCharacterCard);
-
 async function fetchCharacters() {
+  let searchFullName = searchQuery ? "&name=" + encodeURIComponent(searchQuery) : "";
   const response = await fetch(
-    "https://rickandmortyapi.com/api/character?page=" + page
+    "https://rickandmortyapi.com/api/character?page=" + page + searchFullName
   );
   const data = await response.json();
-  //console.log(data);
   let characterData = data.results;
   maxPage = data.info.pages;
-  //console.log(characterData);
   cardContainer.innerHTML = "";
   characterData.forEach((character) => {
     let newCharacterCard = createCharacterCard(character);
@@ -37,7 +32,9 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
-pagination.textContent = page + "/42"; //this needs to be dynamic? how?
+const form = document.querySelector('[data-js="search-bar-container"]');
+
+pagination.textContent = page + "/" + maxPage; //this needs to be dynamic? how?
 
 prevButton.addEventListener("click", () => {
   if (page > 1) {
@@ -53,6 +50,18 @@ nextButton.addEventListener("click", () => {
     fetchCharacters();
     pagination.textContent = page + "/" + maxPage;
   }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  //console.log(data);
+  //console.log(data.query);
+  searchQuery = data.query;
+  page = 1;
+  console.log(searchQuery);
+  fetchCharacters();
 });
 
 // this has to happen before the event listener
